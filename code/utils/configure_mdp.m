4eb00692014a0e2b60d6b5dc8ec38156efcb2ee7
louisfaury <l.faury@hotmail.fr>
2017-04-23 15:53:58 +0200

    Shift configures mode

--- code/utils/configure_mdp.m
+++ code/utils/configure_mdp.m
@@ -84,15 +84,15 @@ switch model_name
         mdp.naive_il.default_value = 0;
         mdp.naive_il.init_lr = 1;
         % Actor critic imitation learning 
-        mdp.ac_il.init_alpha = 8;
-        mdp.ac_il.init_beta = 2;
+        mdp.ac_il.init_alpha = 9;
+        mdp.ac_il.init_beta = 1;
         mdp.ac_il.default_value = 0;
         mdp.ac_il.init_temp = 1;
         mdp.ac_il.stop_criterion = -0.1;
-        mdp.ac_il.temp_mult = 0.95;
+        mdp.ac_il.temp_mult = 0.995;
         mdp.ac_il.max_iter = 400;
         mdp.ac_il.init_lr = 1;
-        mdp.ac_il.eps = 0.5;
+        mdp.ac_il.eps = 0.01;
         % Action value imitation learning 
         mdp.av_il.t0 = 0.2;
         mdp.av_il.qd_init_value = 0;
